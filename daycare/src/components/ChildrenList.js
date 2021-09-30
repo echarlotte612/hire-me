08822de920537e71c6ac630da51c7580a495af94
echarlotte612 <e.charlotte94@gmail.com>
2021-09-30 10:59:55 +0100

    Infinite Scroll added

--- daycare/src/components/ChildrenList.js
+++ daycare/src/components/ChildrenList.js
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import ChildCard from './ChildCard';
 import axios from 'axios';
-import {accessToken, groupId, institutionId} from '../helpers/api';
-
-// const accessToken = '234ffdb8-0889-4be3-b096-97ab1679752c';
-// const groupId = '11fc220c-ebba-4e55-9346-cd1eed714620';
-// const institutionId = 'fb6c8114-387e-4051-8cf7-4e388a77b673';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { accessToken, groupId, institutionId } from '../helpers/api';
 
 export default function ChildrenList() {
     const [children, setChildren] = useState([]);
-    // const [isLoading, setisLoading] = useState(true);
-    // const [length, setLength] = useState(100);
+    const [isLoading, setIsLoading] = useState(true);
+    const [length, setLength] = useState(0);
 
     const getChildren = async () => {
+        setIsLoading(true)
         // GET data from API //
         await axios.get(`https://tryfamly.co/api/daycare/tablet/group?accessToken=${accessToken}&groupId=${groupId}&institutionId=${institutionId}`)
             .then(response => {
-                if(response.status === 200) {
+                if (response.status === 200) {
                     let results = response.data.children
-                    setChildren(results)
-                    console.log(response.data.children)
+                    setLength(results.length)
+                    setChildren([...children, ...results.slice(children.length,children.length+4)]);
+                    console.log(children)
+                    setIsLoading(false)
                 }
                 else {
                     console.err("Couldn't fetch data")
@@ -27,18 +27,39 @@ export default function ChildrenList() {
             })
             .catch(err => {
                 console.error(err)
+                setIsLoading(false)
             })
     }
 
     useEffect(() => {
-        getChildren()
+        getChildren(setChildren)
     }, [])
 
     return (
-        <div className="children-list">
-            {children.map((child, index) =>
-                <ChildCard name={child.name.fullName} key={index} id={child.childId} lastCheckedIn={child.checkinTime} image={child.image.large} />
-            )}
+        <div className="children-list-wrap">
+            <ul className="children-list">
+                <InfiniteScroll
+                    dataLength={children.length} // Length of current array //
+                    next={getChildren} // action to get more data //
+                    hasMore={children.length < length} // decider to call more data: Boolean //
+                    scrollableTarget="app" // Reference to parent node //
+                    className="children-list"
+                    endMessage={ // On data end //
+                        <p className="infinite-scroll-message">
+                            <b>No more records to show.</b>
+                        </p>
+                    } 
+                >
+                    {children.map((child, index) =>
+                        <ChildCard name={child.name.fullName} key={index} id={child.childId} lastCheckedIn={child.checkinTime} image={child.image.large} />
+                    )}
+                </InfiniteScroll>
+            </ul>
+            {isLoading &&
+                <div className="loading">
+                    <p>Loading...</p>
+                </div>
+            }
         </div>
     )
 }
